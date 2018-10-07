125aa0d67376e724365c9e8368834ccfb0f66be2
Brian Birdwell <bbirdwell@gmail.com>
2018-10-07 18:10:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  class TriangleError < StandardError
+  end
+  
+  attr_accessor :kind
+  
+  def initialize(side1, side2, side3)
+    
+  end
+  
+  def kind
+    if !self.valid?
+      return TriangleError
+    elsif self.equilateral?
+      :equilateral
+    end
+  end
+  
+  def valid?
+    if self.side3 > (self.side1 - self.side2) && self.side3 < (self.side1 + self.side2)
+      true
+    else false
+    end
+  end
+    
 end
